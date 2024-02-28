254c2089135bcdb313cd0972912bea051051e462
Roland Schulz <roland.schulz@intel.com>
2024-02-28 13:01:09 -0800

    fix copy trait

--- include/cute/atom/copy_traits_xe.hpp
+++ include/cute/atom/copy_traits_xe.hpp
@@ -7,14 +7,24 @@
 
 namespace cute
 {
-    template<class Tensor>
-    struct Copy_Traits<XE_2D_LOAD, Tensor>
+    template<class GTensor>
+    struct Copy_Traits<XE_2D_LOAD, GTensor>
     {
         //pass (and store) layout and base
         //compute width_minus_one, height_minus_one,  pitch_minus_one from layout
         //Q: should the src (counting) tensor contain the copy tile layout or should the trait contain it and the src only contain the coord? I think it needs to be in the trait
 
-        Tensor tensor;
+        //using ThrID   = Layout<_16>; //TODO: I think it should be 16 (copy is per subggroup) - but static_assert fails
+        using ThrID   = Layout<_1>;
+        using NumBits = Int<sizeof(typename GTensor::engine_type::value_type)*8>;
+        // Map from (src-thr,src-val) to bit
+        using SrcLayout = Layout<Shape<_1,NumBits>>;
+        // Map from (dst-thr,dst-val) to bit
+        using DstLayout = Layout<Shape<_1,NumBits>>;
+        // Reference map from (thr,val) to bit
+        using RefLayout = SrcLayout;
+
+        GTensor tensor;
 
         template <class TS, class SLayout,
                   class TD, class DLayout>
@@ -23,17 +33,17 @@ namespace cute
                     Tensor<TS, SLayout> const &src,
                     Tensor<TD, DLayout> &dst)
         {
-            int H = size<0>(tensor);
-            int W = size<1>(tensor) * sizeof(tensor::engine_type::value_type);
-            XE_2D_LOAD::copy(tensor.data(), W, H, W, src.data().coord_, dst.data());
+            int H = size<0>(traits.tensor);
+            int W = size<1>(traits.tensor) * sizeof(decltype(traits.tensor)::engine_type::value_type);
+            XE_2D_LOAD::copy(traits.tensor.data(), W, H, W, src.data().coord_, dst.data());
         }
     };
 
-    template<class Engine, class Layout>
+    template<class GEngine, class GLayout>
     auto make_xe_2d_copy(Tensor<GEngine, GLayout> gtensor) {
         using GTensor = Tensor<GEngine, GLayout>;
-        using Traits = Copy_Traits<XE_2D_LOAD, GTensor>
-        Traits traits{tensor};
+        using Traits = Copy_Traits<XE_2D_LOAD, GTensor>;
+        Traits traits{gtensor};
         return Copy_Atom<Traits, typename GEngine::value_type>{traits};
     }
 }
--- include/cute/util/sycl_vec.hpp
+++ include/cute/util/sycl_vec.hpp
@@ -12,6 +12,6 @@ template<class T, int N> using vector_t = sycl::vec<T,N>;
 using float8 = vector_t<float, 8>;
 using short8 = vector_t<short, 8>;
 using ushort8 = vector_t<ushort, 8>;
-// using int2 = vector_t<int, 2>; //conflicts with vector_types
+using int2_ = vector_t<int, 2>; //conflicts with vector_types
 using int8 = vector_t<int, 8>;
 using uint8 = vector_t<uint, 8>;
