9bdc5c59ea6f3b6c0d7b1719b74bab5e81af66ae
Alejandro Acosta <alejandro.acosta@codeplay.com>
2024-08-05 12:36:06 +0100

    Use __SYCL_DEVICE_ONLY__ instead of CUTLASS_ENABLE_SYCL (#111)

--- include/cutlass/tfloat32.h
+++ include/cutlass/tfloat32.h
@@ -107,7 +107,7 @@ struct alignas(4) tfloat32_t {
 //  explicit tfloat32_t(int x) {
   tfloat32_t(int x) {
     float flt = static_cast<float>(x);
-    #if defined(__CUDA_ARCH__) || defined(CUTLASS_ENABLE_SYCL)
+    #if defined(__CUDA_ARCH__) || defined(__SYCL_DEVICE_ONLY__)
     storage = reinterpret_cast<uint32_t const &>(flt);
     #else
     std::memcpy(&storage, &flt, sizeof(storage));
@@ -122,7 +122,7 @@ struct alignas(4) tfloat32_t {
     // of the mantissa.
     unsigned bits = (storage & ~0x1fffu);
 
-    #if defined(__CUDA_ARCH__) || defined(CUTLASS_ENABLE_SYCL)
+    #if defined(__CUDA_ARCH__) || defined(__SYCL_DEVICE_ONLY__)
     return reinterpret_cast<float const &>(bits);
     #else
     float flt;
